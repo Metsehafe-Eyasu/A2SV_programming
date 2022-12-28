78bd1fe4dcd3de88eeda45858d717e651e04cfe6
Metsehafe-Eyasu <metsehafe.eyasu@aastustudent.edu.et>
2022-12-28 14:03:43 +0300

    python if else

---
+++ problems/HackerRank/python-if-else.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 == 1:
+        print('Weird')
+    else:
+        if n < 6:
+            print('Not Weird')
+        elif n >5 and n < 21:
+            print('Weird')
+        else:
+            print('Not Weird')
