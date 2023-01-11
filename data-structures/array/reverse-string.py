e432ea34ba6ae933e5b9d72f87713b2e69c42b19
Metsehafe-Eyasu <metsehafe.eyasu@aastustudent.edu.et>
2023-01-11 18:34:44 +0300

    start of data structures

---
+++ data-structures/array/reverse-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            right -= 1
+            left += 1
+         
