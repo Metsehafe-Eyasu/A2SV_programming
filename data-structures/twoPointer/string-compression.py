d52159aba9bc1bfa5cbeb65678420762fb1b1643
Metsehafe-Eyasu <metsehafe.eyasu@aastustudent.edu.et>
2023-02-01 20:11:56 +0300

    string compression: in place

--- data-structures/twoPointer/string-compression.py
+++ data-structures/twoPointer/string-compression.py
@@ -1,24 +1,31 @@
-
 class Solution:
-    def compress(self, chars: List[str]) -> int:
+    def compress(self, chars: list[str]) -> int:
         length = len(chars)
         initial = chars[0]
-        answer = []
+        index = 0
         count = 0
         for i in range(length + 1):
             if i < length and initial == chars[i]:
                 count += 1
             else:
-                answer.append(initial)
+                chars[index] = initial
+                index += 1
 
                 if i < length:
                     initial = chars[i]
                 
                 if count > 1:
-                    answer.extend([*str(count)])
+                    for j in [*str(count)]:
+                        chars[index] = j
+                        index += 1
                 count = 1
-        chars.clear()
-        for i in answer:
-            chars.append(i)
-        return len(answer)
+        print(chars)
+        for k in range(length-index):
+            chars.pop()
+        print(chars)
+        return len(chars)
+
             
+ans = Solution()
+chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+ans.compress(chars)
