a7284dfa5c09e26370ea2d8ea861bbc85caebace
Metsehafe Eyasu Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-02-06 17:56:56 +0300

    african crossword

--- data-structures/array/african-crossword.py
+++ data-structures/array/african-crossword.py
@@ -1,8 +1,24 @@
 row, col = list(map(int, input().split()))
 matrix = [[*input()] for _ in range(row)]
 
-print(*matrix, sep="\n")
+# print(*matrix, sep="\n")
+
+answer = []
 
 for i in range(row):
     for j in range(col):
+        noDuplicates = True
+        for k in range(row):
+            if k != i and matrix[i][j] == matrix[k][j]:
+                noDuplicates = False
+                break
+        if not noDuplicates:
+            continue
+        for k in range(col):
+            if k != j and matrix[i][j] == matrix[i][k]:
+                noDuplicates = False
+                break
+        if noDuplicates:
+            answer.append(matrix[i][j])
+print(*answer, sep="")
         
