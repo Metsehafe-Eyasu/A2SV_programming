30ffc58a41315d5304e17fd9cddc4c34f977d795
Metsehafe Eyasu Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-02-10 19:16:44 +0300

    middle of the linked list

---
+++ data-structures/linkedList/middle-of-linked-list.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        turtle = head
+        rabbit = head
+        while rabbit and rabbit.next:
+            turtle = turtle.next
+            rabbit = rabbit.next.next
+        return turtle
