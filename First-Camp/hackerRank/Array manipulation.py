161da1e19d099d46b21f5f4adf8310f7b8ca03e0
Metsehafe Eyasu Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-02-23 10:47:45 +0300

    array manipulation

---
+++ First-Camp/hackerRank/Array manipulation.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    holder = [0] * (n+1)
+    for i, j, val in queries:
+        holder[i - 1] += val
+        holder[j] -= val
+    maxVal = 0
+    prevVal = 0
+    for num in holder:
+        prevVal = num + prevVal
+        maxVal = max(maxVal, prevVal)
+    return maxVal
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
