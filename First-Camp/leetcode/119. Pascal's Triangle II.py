22fc794e9f65c3936310dfc6b0451763c80f0935
Metsehafe Eyasu Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-02-28 21:33:03 +0300

    pascal's triangle

---
+++ First-Camp/leetcode/119. Pascal's Triangle II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        prevRow = self.getRow(rowIndex - 1)
+        currRow = [1]
+        for i in range(1, rowIndex):
+            currRow.append(prevRow[i] + prevRow[i - 1])
+        currRow.append(1)
+        return currRow
