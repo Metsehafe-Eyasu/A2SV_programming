a0f3000ce3296a1a310fab94f6f8bf203c16d246
Metsehafe Eyasu Tesfu <118991565+Metsehafe-Eyasu@users.noreply.github.com>
2023-03-03 19:11:47 +0300

    Create 930. Binary Subarrays With Sum.py

---
+++ First-Camp/leetcode/930. Binary Subarrays With Sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def atMost(self, nums: List[int], k: int) -> int:
+        if k < 0:
+            return 0
+        length = len(nums)
+        ans = left = 0
+        for right in range(length):
+            k -= nums[right]
+            while k < 0:
+                k += nums[left]
+                left += 1
+            ans += right - left + 1
+        return ans
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        return self.atMost(nums, goal) - self.atMost(nums, goal - 1)       
