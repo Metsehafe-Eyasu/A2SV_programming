9c4a5ae221a1ad79b354386c51790cf610d0aa23
Metsehafe Eyasu Tesfu <118991565+Metsehafe-Eyasu@users.noreply.github.com>
2023-03-03 19:05:22 +0300

    Create 50. Pow(x, n).py

---
+++ First-Camp/leetcode/50. Pow(x, n).py
@@ -0,0 +1,11 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 1: return x
+        if n == 0: return 1
+        if n < 0:
+            return 1/(self.myPow(x, abs(n)))
+        else:
+            if n%2 == 0:
+                return self.myPow(x*x, n/2)
+            else:
+                return x*self.myPow(x*x, (n-1)/2)
