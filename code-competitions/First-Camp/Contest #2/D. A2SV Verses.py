2ab487d46dec8325e611bab86c2facc3fc829c52
Metsehafe Eyasu Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-03-05 02:58:19 +0300

    4th question of camp contest #2

--- code-competitions/First-Camp/Contest #2/D. A2SV Verses.py
+++ code-competitions/First-Camp/Contest #2/D. A2SV Verses.py
@@ -1,17 +1,16 @@
-from itertools import accumulate
 students, minSum, maxSum = list(map(int, input().split()))
-
 studList = list(map(int, input().split()))
-prefix = list(accumulate(studList, initial=0))
-left = 0
-groups = 0
-for right in range(students+1):
-    if prefix[right] - prefix[left] <= maxSum and prefix[right] - prefix[left] >= minSum:
-            groups += 1
-            
-    while left < right and prefix[right] - prefix[left] >= minSum:
-        left += 1
-        if prefix[right] - prefix[left] <= maxSum and prefix[right] - prefix[left] >= minSum:
-            groups += 1
-    
-print(groups)   
+
+def atMost(nums, k) -> int:
+    length = len(nums)
+    if k < 0: return 0
+    left = ans = 0
+    for right in range(length):
+        k -= nums[right]
+        while k < 0:
+            k += nums[left]
+            left += 1
+        ans += right - left + 1
+    return ans
+
+print(atMost(studList, maxSum) - atMost(studList, minSum - 1)) 
