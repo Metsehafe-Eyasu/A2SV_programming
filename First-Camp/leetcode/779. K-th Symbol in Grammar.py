edb1af7ce58f5e072e2b51fe1050b6833c6a1058
Metsehafe Eyasu Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-03-06 15:06:37 +0300

    kth symbol

---
+++ First-Camp/leetcode/779. K-th Symbol in Grammar.py
@@ -0,0 +1,6 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1: return 0
+        
+        if k <= pow(2, n-2): return self.kthGrammar(n-1, k)
+        else: return 1 - self.kthGrammar(n-1, k - pow(2, n-2))
