626bee5a82e79d527dc2b852cb5df499550d2171
Metsehafe Eyasu Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-03-08 12:12:40 +0300

    BFS (level order traversal)

---
+++ First-Camp/leetcode/102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue = deque()
+        ans = []
+        if not root:
+            return ans
+        queue.append(root)
+        while queue:
+            level = []
+            length = len(queue)
+            while length:
+                curr = queue.popleft()
+                level.append(curr.val)
+
+                if curr.left:queue.append(curr.left)
+                if curr.right:queue.append(curr.right)
+                length -= 1
+            ans.append(level)
+        return ans
