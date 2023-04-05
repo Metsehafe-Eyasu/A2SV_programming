d2c87360bc3187d0108f043be392f98bbf6b88d2
Metsehafe Tesfu <thephenompro@gmail.com>
2023-04-05 19:02:31 +0300

    GCD codeforces

---
+++ Post_camp_Problems/codeforces/complicated_GCD.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define ll long long
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define MOD 1000000007
+
+/**
+ * @author ThePhenom-Pro: Metsehafe-Eyasu
+ * @brief Custom template for CodeForces
+*/
+
+
+void solution() {
+    string a, b;
+    cin >> a >> b;
+    if (a == b) cout << a;
+    else cout << 1;
+}
+
+int main() {
+    fast;
+    solution();
+}
