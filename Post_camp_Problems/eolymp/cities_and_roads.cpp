7ce230fe474efb6f837d478d9f8ce9b9c01d8c90
Metsehafe Tesfu <thephenompro@gmail.com>
2023-04-12 17:22:01 +0300

    cities and roads

---
+++ Post_camp_Problems/eolymp/cities_and_roads.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<ll>
+#define vpii vector<pii>
+#define FOR_(i, n) for (int i = 0; i < n; ++i)
+#define FORR_(i, n) for (int i = n - 1; i >= 0; --i)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define INF 1e9
+#define MOD 1000000007
+
+/**
+ * @author ThePhenom-Pro: Metsehafe-Eyasu
+ * @brief Custom template for CodeForces
+ */
+
+// Display list: Debugging purposes
+template <typename T>
+void display(vector<T> &arr)
+{
+    for (auto &i : arr)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+// Input list: customizable
+template <typename T>
+void inputList(vector<T> &arr, int n)
+{
+    arr.resize(n);
+    for (auto &a : arr)
+        cin >> a;
+}
+
+// Main function for solving the problem
+void solve()
+{
+    // Start here
+    int n;
+    cin >> n;
+    vector<vector<int>> grid(n);
+    FOR_(i, n) inputList(grid[i], n);
+    int count = 0;
+    FOR_(i, n) {
+        for(int j = i+1; j < n; j++) {
+            if (grid[i][j] == 1) count++;
+        }
+    } 
+    cout << count << endl;
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
