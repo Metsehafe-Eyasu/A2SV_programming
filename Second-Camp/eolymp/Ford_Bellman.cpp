bb88860e33aeb47ec2676c41a5c31582f9ae12bd
Metsehafe Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-10-04 10:51:49 +0300

    bellman ford

---
+++ Second-Camp/eolymp/Ford_Bellman.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shorthand notations
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<ll, ll> pll;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef priority_queue<int, vector<int>, greater<int>> min_pq;
+typedef priority_queue<int> max_pq;
+
+// Macros
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i, n) for (int i = 0; i < n; ++i)
+#define FORR(i, n) for (int i = n - 1; i >= 0; --i)
+#define forit(it, c) for (__typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+#define forRange(i, a, b) for (int i = (a); i <= (b); ++i)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)x.size()
+#define INF 1e9
+#define MOD 1000000007
+
+/**
+ * @author ThePhenom-Pro: Metsehafe-Eyasu
+ * @brief Custom template for CodeForces
+ */
+template <typename T>
+void display(vector<T> &arr) {
+    for (auto &i : arr) cout << i << " ";
+    cout << endl;
+}
+
+template <typename T>
+void inputList(vector<T> &arr, int n) {
+    arr.resize(n);
+    for (auto &a : arr) cin >> a;
+}
+
+void ford(int n, vector<vector<int>>& edges) {
+    vector<int>dist(n+1, 30000);
+    dist[1] = 0;
+    FOR(_, n-1) {
+        FOR(i, edges.size()) {
+            int from = edges[i][0],
+                to = edges[i][1],
+                weight = edges[i][2];
+            if (dist[from] != 30000 && dist[from]+weight < dist[to])
+                dist[to] = dist[from] + weight;
+        }
+    }
+    forRange(i, 1, n) {
+      cout << dist[i] << " ";
+    }
+    cout << endl;
+}
+
+// Main function for solving the problem
+void solve() {
+    // Start here
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>>edges;
+    FOR(_, m) {
+      int from, to, weight;
+      cin >> from >> to >> weight;
+      edges.emplace_back(vector<int>{from, to, weight});
+    }
+    ford(n, edges);
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
