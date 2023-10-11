05c3563ba9c3e0a778082c4ca5d4c72ac427d579
Metsehafe Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2023-10-11 17:08:59 +0300

    706 design hashmap complete

---
+++ Second-Camp/leetcode/706. Design HashMap.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyHashMap {
+public:
+    vector<int>hashmap;
+    MyHashMap() : hashmap(1e6+1, -1) {}
+    
+    void put(int key, int value) {
+        hashmap[key] = value;
+    }
+    
+    int get(int key) {
+        return hashmap[key];
+    }
+    
+    void remove(int key) {
+        hashmap[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
