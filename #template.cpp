b0c6f6069bca04c469e5e4fbf8438e5fc30e23f1
Metsehafe-Eyasu <118991565+Metsehafe-Eyasu@users.noreply.github.com>
2024-04-19 13:42:46 +0300

    C++ template update

--- #template.cpp
+++ #template.cpp
@@ -7,25 +7,16 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<ll> vl;
 typedef vector<pii> vpii;
-typedef vector<string> vs;
-typedef vector<double> vd;
-typedef vector<bool> vb;
 typedef vector<vi> vvi;
 typedef vector<vl> vvl;
 typedef pair<ll, ll> pll;
-typedef set<int> si;
-typedef set<ll> sl;
 
-template<typename T>
-using min_pq = priority_queue<T, vector<T>, greater<T>>;
-template<typename T>
-using max_pq = priority_queue<T>;
-template<typename Key, typename Value>
-using umap = unordered_map<Key, Value>;
-template<typename Key1, typename Key2, typename Value>
-using umumap = unordered_map<Key1, unordered_map<Key2, Value>>;
-template<typename T>
-using vv = vector<vector<T>>;
+template<typename T> using min_pq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using max_pq = priority_queue<T>;
+template<typename Key, typename Value> using umap = unordered_map<Key, Value>;
+template<typename Key1, typename Key2, typename Value> using umumap = unordered_map<Key1, unordered_map<Key2, Value>>;
+template<typename T> using vv = vector<vector<T>>;
+template<typename T> using v = vector<T>;
 
 // Macros
 #define FOR(i, n) for (int i = 0; i < n; ++i)
@@ -34,40 +25,23 @@ using vv = vector<vector<T>>;
 #define forRange(i, a, b) for (int i = (a); i <= (b); ++i)
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
-#define pb push_back
-#define mp make_pair
-#define sz(x) (int)x.size()
-#define INF 1e9
-#define MOD 1000000007
+#define MOD 1'000'000'007
 
-void YES() {
-    cout << "YES\n";
-}
-void NO() {
-    cout << "NO\n";
-}
+void YES() { cout << "YES\n"; }
+void NO() { cout << "NO\n"; }
+
+template <typename T> void display(vector<T> &arr) { for (auto &i : arr) cout << i << " "; cout << endl; }
+template <typename T> void IL(vector<T> &arr, int n) { arr.resize(n); for (auto &a : arr) cin >> a; }
 
 /**
- * @author ThePhenom-Pro: Metsehafe-Eyasu
- * @brief Custom template for CodeForces
+ * @author ThePhenom-Pro
  */
 
-template <typename T>
-void display(vector<T> &arr) {
-    for (auto &i : arr) cout << i << " ";
-    cout << endl;
-}
-
-template <typename T>
-void IL(vector<T> &arr, int n) {
-    arr.resize(n);
-    for (auto &a : arr) cin >> a;
-}
 
 // Main function for solving the problem
 void solve() {
     // Start here
-    
+
 }
 
 int main() {
